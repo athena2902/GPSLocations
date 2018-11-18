f4ab0336cdb2cc60c16d8635aa11c067a3adbb9b
Quyet Vu <quyetvv_55@vnu.edu.vn>
2018-11-18 21:32:30 +0700

    Initial Commit

---
+++ GPSLocations/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  GPSLocations
+//
+//  Created by Quyet Vu on 11/18/18.
+//  Copyright © 2018 Quyet Vu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
