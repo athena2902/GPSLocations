6e78a096bf109e7f51eacfefd82fd3f11606597d
Quyet Vu <quyetvv_55@vnu.edu.vn>
2018-11-21 22:39:38 +0700

    Tag location

--- GPSLocations/CurrentLocationViewController.swift
+++ GPSLocations/CurrentLocationViewController.swift
@@ -34,6 +34,16 @@ class CurrentLocationViewController: UIViewController, CLLocationManagerDelegate
         super.viewDidLoad()
         updateLabels()
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.isNavigationBarHidden = false
+    }
 
     @IBAction func getLocation() {
         let authStatus = CLLocationManager.authorizationStatus()
@@ -236,6 +246,15 @@ class CurrentLocationViewController: UIViewController, CLLocationManagerDelegate
             stopLocationManager()
             lastLocationError = NSError(domain: "MyLocationsErrorDomain", code: 1, userInfo: nil)
             updateLabels()
+        }
+    }
+    
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "TagLocation" {
+            let controller = segue.destination as! LocationDetailsViewController
+            controller.coordinate = location!.coordinate
+            controller.placemark = placemark
         }
     }
 }
